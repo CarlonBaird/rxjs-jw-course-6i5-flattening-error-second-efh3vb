e68cbd6af3fc1b95f407509ff9d800e945a91133
CarlonBaird <carlyestrella@live.com>
2022-11-25 10:43:59 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,18 +1,26 @@
-import { EMPTY, fromEvent } from "rxjs";
-import { ajax } from "rxjs/ajax";
-import { catchError, concatMap, map } from "rxjs/operators";
+import { EMPTY, fromEvent, of } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { catchError, concatMap, map } from 'rxjs/operators';
 
-const endpointInput: HTMLInputElement = document.querySelector('input#endpoint');
+const endpointInput: HTMLInputElement =
+  document.querySelector('input#endpoint');
 const fetchButton = document.querySelector('button#fetch');
 
-fromEvent(fetchButton, 'click').pipe(
-  map(() => endpointInput.value),
-  concatMap(value =>
-    ajax(`https://random-data-api.com/api/${value}/random_${value}`)
-  ),
-  catchError(() => EMPTY)
-).subscribe({
-  next: value => console.log(value),
-  error: err => console.log('Error:', err),
-  complete: () => console.log('Completed')
-});
+//search using the following: food, incorrect-endpoint
+
+fromEvent(fetchButton, 'click')
+  .pipe(
+    map(() => endpointInput.value),
+    concatMap((value) =>
+      ajax(`https://random-data-api.com/api/${value}/random_${value}`).pipe(
+        // catchError(() => EMPTY)
+        //If we want to inform the user that the endpoint is valid, we can do the following:
+        catchError((error) => of(`Could not fetch data: ${error}`))
+      )
+    )
+  )
+  .subscribe({
+    next: (value) => console.log(value),
+    error: (err) => console.log('Error:', err),
+    complete: () => console.log('Completed'),
+  });
